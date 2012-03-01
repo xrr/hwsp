2ee4be763a29b36cb3e15880403e441e4f0d5538
romain.rousseau@gmail.com <romain.rousseau@gmail.com@66af7bcb-98a0-0701-428a-0072433cbb04>
2012-03-01 15:50:36 +0000

    Hull-White trinomial tree: construction stage 2 implemented

--- Interface/Interface.cpp
+++ Interface/Interface.cpp
@@ -5,15 +5,26 @@
 
 int main() {
 
-	std::vector<double> dt;
-	for (int i = 0 ; i<3 ; i++) dt.push_back(1);
+	Curve courbe;
+
+	for (int i = 0 ; i<3 ; i++) courbe.times.push_back(1+i);
+	courbe.rates.push_back(0.0382365);
+	courbe.rates.push_back(0.0451162);
+	courbe.rates.push_back(0.0508626);
+
+	//for (int i = 0 ; i<4 ; i++) courbe.times.push_back(1+i);
+	//courbe.rates.push_back(0.05092755);
+	//courbe.rates.push_back(0.05795397);
+	//courbe.rates.push_back(0.06304557);
+	//courbe.rates.push_back(0.06733466);
 
 	HullWhite hw;
 	hw.a = 0.1;
 	hw.sigma = 0.01;
+	hw.ratecurve = courbe;
 
-	Tree arbre(dt,hw);
+	Tree arbre(hw);
 	arbre.construct();
-	std::cout << arbre;
+	std::cout << "\n" << arbre;
 	std::getchar();
 }
--- Lib/Node.h
+++ Lib/Node.h
@@ -17,9 +17,25 @@ std::string fixed_percentage (double value, int precision = 3) {
 	return out.str();
 };
 
+class Curve {
+public:
+	std::vector<double> times;
+	std::vector<double> rates;
+	double zerocoupon(double time) {
+		double prev_time = 0, tmp = 0;
+		unsigned int k=0; while((k < times.size()) && (times[k]<=time)) {
+			tmp+=rates[k]*(times[k]-prev_time);
+			prev_time = times[k];
+			k++;
+		};
+		return exp(-tmp);
+	};
+};
+
 class HullWhite {
 public:
 	double a, sigma;
+	Curve ratecurve;
 };
 
 class Node;
@@ -31,12 +47,15 @@ public:
 	Transition(void) : probability(NULL), destination(NULL) {};
 	Transition(double _probability) : probability(_probability), destination(NULL) {};
 };
+
 class Node {
 public:
 	int relative_position;
-	double x, r; // displaced, normal rates
+	double x, alpha, q; // no drift rate, displacement, arrow_debreu
 	Transition transitions[3]; // down, middle, up
-	Node(int _relative_position, double _x) : relative_position(_relative_position), x(_x), r(NULL) {};
+	double r(void) { return x+alpha; }; // displaced rate
+	Node(int _relative_position, double _x) : relative_position(_relative_position), x(_x), alpha(NULL), q(0) {};
+	Node(int _relative_position, double _x, double _q, double _alpha) : relative_position(_relative_position), x(_x), alpha(_alpha), q(_q) {};
 };
 
 class Tree {
@@ -46,20 +65,21 @@ private:
 	std::vector<std::vector<Node>> dates;
 
 public:
-	std::vector<double> timesteps;
 
 	friend std::ostream& operator<< (std::ostream&, Tree&);
 
-	Tree (std::vector<double> _timesteps, HullWhite hw) : hw_(hw), timesteps(_timesteps) {
+	Tree (HullWhite hw) : hw_(hw) {
 		std::vector<Node> date0;
-		date0.push_back(Node(0,0));
+		date0.push_back(Node(0,0,1,hw_.ratecurve.rates[0]));
 		dates.push_back(date0);
 	};
 
 	void construct(void) {
-		for (std::vector<double>::iterator timestep = timesteps.begin(); timestep != timesteps.end(); timestep++) {
+		double prev_time = 0;
+		for (auto time = hw_.ratecurve.times.begin(); time != hw_.ratecurve.times.end()-1; time++) {
 			std::vector<Node> newdate;
-			double e = exp(-hw_.a*(*timestep));
+			double delta_t = *time-prev_time;
+			double e = exp(-hw_.a*delta_t);
 			double V = hw_.sigma * sqrt((1-e*e)/(2*hw_.a));
 			double delta_x = V * sqrt(3.0);
 			for (auto currentnode = dates.back().begin(); currentnode != dates.back().end(); currentnode++) {
@@ -78,9 +98,16 @@ public:
 						newtransition.destination=&newdate.back();
 					}
 					currentnode->transitions[1+l]=newtransition;
+					newtransition.destination->q+=currentnode->q*newtransition.probability*exp(-(currentnode->alpha+currentnode->x)*delta_t);
 				}
 			}
 			dates.push_back(newdate);
+			double alpha = 0;
+			for (auto currentnode = dates.back().begin(); currentnode != dates.back().end(); currentnode++) alpha+=currentnode->q*exp(-currentnode->x*delta_t);
+			alpha = log(alpha/(hw_.ratecurve.zerocoupon(*(time+1))))/delta_t;
+			std::cout << fixed_percentage(alpha) << "\n";
+			for (auto currentnode = dates.back().begin(); currentnode != dates.back().end(); currentnode++) currentnode->alpha = alpha;
+			prev_time = *time;
 		}
 	}
 };
@@ -88,7 +115,11 @@ public:
 std::ostream& operator<< (std::ostream& flux, Tree& tree) {
 	for (auto currentdate = tree.dates.begin(); currentdate != tree.dates.end(); currentdate++) {
 		for (auto currentnode = currentdate->begin(); currentnode != currentdate->end(); currentnode++) {
-			flux << fixed_percentage(currentnode->x) << " ";
+			flux << fixed_percentage(currentnode->r()) << " ";
+			/*flux << fixed_percentage(currentnode->transitions[2].probability) << " "
+				<< fixed_percentage(currentnode->transitions[1].probability) << " "
+				<< fixed_percentage(currentnode->transitions[0].probability) << " ";
+			flux << "\n";*/
 		}
 		flux << "\n";
 	}
