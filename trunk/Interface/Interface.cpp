14f39cc7de3c60a6d4d7463f0a72aa05244494a3
romain.rousseau@gmail.com <romain.rousseau@gmail.com@66af7bcb-98a0-0701-428a-0072433cbb04>
2012-03-03 15:37:02 +0000

    class hierarchy refounded

--- trunk/Interface/Interface.cpp
+++ trunk/Interface/Interface.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <fstream>
-
-#include "Node.h"
 #include <math.h>
+
+#include "Models.h"
+#include "Pricers.h"
 
 int main() {
 
-	Curve courbe;
+	RateCurve courbe;
 
 	for (int i = 0 ; i<100 ; i++) {
 		courbe.times.push_back(0.2*(1+i));
 		courbe.rates.push_back(0.08-0.05*exp(-0.18*(0.2*(1+i))));
 	}
 
-	Tree arbre(HullWhite(0.1, 0.01, courbe));
-	arbre.construct();
+	Tree arbre(courbe,HullWhite(0.1, 0.01));
 	
 	std::ofstream(std::ofstream("arbre.csv", std::ios::trunc)) << arbre;
-	
 	std::cout << "\n" << arbre;
 
-	
 	std::getchar();
+	return 0;
 }
 
---
+++ trunk/Lib/Pricers.h
@@ -0,0 +1,145 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <boost/math/special_functions/round.hpp>
+
+#include "Models.h"
+#include "Payoffs.h"
+#include "Tools.h"
+
+//#include <algorithm>
+
+//todo = check transition pointers in class Tree
+
+
+class PricerGeneric {
+public :
+	RateCurve ratecurve;
+	PricerGeneric(RateCurve _ratecurve) : ratecurve(_ratecurve) {};
+
+	virtual double Evaluate(Swap swap) {
+		// formule fermee
+		// TODO
+		return -999;
+	}
+};
+
+class PricerOptions : public PricerGeneric {
+public:
+	HullWhite hullwhite;
+	PricerOptions(RateCurve _ratecurve, HullWhite _hullwhite) : PricerGeneric(_ratecurve), hullwhite(_hullwhite) {};
+
+	virtual double Evaluate(Swaption)=0;
+};
+
+
+class Node;
+
+class Transition {
+public:
+	double probability;
+	Node *destination;
+	Transition(void) : probability(NULL), destination(NULL) {};
+	Transition(double _probability) : probability(_probability), destination(NULL) {};
+};
+
+class Node {
+public:
+	int relative_position;
+	double x, alpha, q; // no drift rate, displacement, arrow_debreu
+	Transition transitions[3]; // down, middle, up
+	double r(void) { return x+alpha; }; // displaced rate
+	Node(int _relative_position, double _x) : relative_position(_relative_position), x(_x), alpha(NULL), q(0) {};
+	Node(int _relative_position, double _x, double _q, double _alpha) : relative_position(_relative_position), x(_x), alpha(_alpha), q(_q) {};
+};
+
+class Tree : public PricerOptions {
+
+private:
+	std::vector<std::vector<Node>> slices;
+
+public:
+
+	friend std::ostream& operator<< (std::ostream&, Tree&);
+
+	virtual double Evaluate(Swaption swaption) {
+		//todo;
+		return -999;
+	};
+
+	Tree (RateCurve _ratecurve, HullWhite _hullwhite) : PricerOptions(_ratecurve, _hullwhite) {
+		std::vector<Node> slice0;
+		slice0.push_back(Node(0,0,1,ratecurve.rates[0]));
+		slices.push_back(slice0);
+		double prev_time = 0;
+		for (auto time = ratecurve.times.begin(); time != ratecurve.times.end()-1; time++) {
+			std::vector<Node> newslice;
+			double delta_t = *time-prev_time;
+			double e = exp(-hullwhite.a*delta_t);
+			double V = hullwhite.sigma * sqrt((1-e*e)/(2*hullwhite.a));
+			double delta_x = V * sqrt(3.0);
+			for (auto currentnode = slices.back().begin(); currentnode != slices.back().end(); currentnode++) {
+				double M = currentnode->x*e;			
+				int k = boost::math::iround(M/delta_x);
+				double R = (M-k*delta_x)/V; // eta/V
+				for (int l=-1; l<=+1; l++) {
+					Transition newtransition((1+R*R)/6.0+((0==l)?(1-R*R):l*R/sqrt(3.0))/2.0);
+					auto existingnode = newslice.begin();
+					while (existingnode != newslice.end() && newtransition.destination == NULL) {
+						if (existingnode->relative_position == k+l) newtransition.destination = &(*existingnode);
+						existingnode++;
+					}
+					if (newtransition.destination == NULL) {
+						newslice.push_back(Node(k+l, (k+l)*delta_x));
+						newtransition.destination=&newslice.back();
+					}
+					currentnode->transitions[1+l]=newtransition;
+					newtransition.destination->q+=currentnode->q*newtransition.probability*exp(-currentnode->r()*delta_t);
+				}
+			}
+			slices.push_back(newslice);
+			double alpha = 0;
+			for (auto currentnode = slices.back().begin(); currentnode != slices.back().end(); currentnode++) alpha+=currentnode->q*exp(-currentnode->x*delta_t);
+			alpha = log(alpha/(ratecurve.zerocoupon(*(time+1))))/delta_t;
+			std::cout << fixed_percentage(alpha) << "\n";
+			for (auto currentnode = slices.back().begin(); currentnode != slices.back().end(); currentnode++) currentnode->alpha = alpha;
+			prev_time = *time;
+		}
+	};
+
+};
+
+std::ostream& operator<< (std::ostream& flux, Tree& tree) {
+	for (auto currentslice = tree.slices.begin(); currentslice != tree.slices.end(); currentslice++) {
+		for (auto currentnode = currentslice->begin(); currentnode != currentslice->end(); currentnode++) {
+			flux << fixed_percentage(currentnode->r(),5) << "; ";
+			/*flux << fixed_percentage(currentnode->transitions[2].probability) << " "
+			<< fixed_percentage(currentnode->transitions[1].probability) << " "
+			<< fixed_percentage(currentnode->transitions[0].probability) << " ";
+			flux << "\n";*/
+		}
+		flux << "\n";
+	}
+	return flux;
+}
+
+class ClosedFormula : public PricerOptions {
+private :
+	double B(double t, double T) {return 1/hullwhite.a*(1-exp(-hullwhite.a*(T-t)));};
+
+	double A(double t, double T) {
+		//todo : ecrire une fonction rate(t) semblable a Zerocoupon(t) (en attendant : valeur bidon = rates[1])
+		return ratecurve.zerocoupon(T)/ratecurve.zerocoupon(t)*exp(
+			B(t,T)*ratecurve.rates[0]-hullwhite.sigma*hullwhite.sigma/(4*hullwhite.a)*(1-exp(-2*hullwhite.a)*B(t,T)*B(t,T)));
+	};
+
+public :
+	ClosedFormula(RateCurve _ratecurve, HullWhite _hullwhite) : PricerOptions(_ratecurve, _hullwhite) {};
+
+	virtual double Evaluate(Swaption swaption) {
+		//todo;
+		return -999;
+	};
+
+};
