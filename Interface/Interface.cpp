b679220c8ddd565459268b23e30765008c310222
romain.rousseau@gmail.com <romain.rousseau@gmail.com@66af7bcb-98a0-0701-428a-0072433cbb04>
2012-03-07 01:50:39 +0000

    Tree visualization implemented (GraphViz DOTS output format) Node pointers bag-of-bones resolved ! (std::vector<std::vector<Node*>>) ...

--- Interface/Interface.cpp
+++ Interface/Interface.cpp
@@ -1,41 +1,51 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+
 #include <math.h>
 
-#include "Models.h"
 #include "Pricers.h"
-
-//concatenate a 1st vector of dates with the vector of swap dates (start & payments)
-/*Tree(RateCurve _ratecurve, HullWhite _hullwhite, Swap swap) :  PricerOptions(_ratecurve, _hullwhite) {
-Dates blabla;
-Tree(_ratecurve, _hullwhite, blabla);
-};*/
-
-//std::ofstream(std::ofstream("arbre.csv", std::ios::trunc)) << arbre;
-//std::cout << "\n" << arbre;
+#include "Tools.h"
 
 
-int main() {
+int main () {
 
 	RateCurve courbe;
 	for (int i = 0 ; i<3 ; i++) {
 		courbe.times.push_back(2+i);
 		courbe.rates.push_back(0.05);
 	}
-	HullWhite hullwhite(0.05, 0.01);
+
+	std::vector<double> tree_dates;
+	for (int i = 0 ; i<4 ; i++) tree_dates.push_back(0.2*(1+i));
+	tree_dates.insert(tree_dates.end(), courbe.times.begin(), courbe.times.end());
+
+	HullWhite hullwhite(0.1, 0.01);
+
 
 	PricerGeneric pricer_swap(courbe);
 	ClosedFormula pricer_option1(courbe, hullwhite);
-
-	Swap swap(0.05,1,courbe.times);
-	Swaption swaption1(swap, true);
-	Swaption swaption2(swap, false);
+	Tree pricer_option2(courbe,hullwhite, tree_dates);
 
 
-	std::cout << "=== Formule fermee ==="  << "\n";
-	std::cout << "Payeur \t\t: " << percentage(pricer_swap.Evaluate(swap)) << "\n";
-	std::cout << "Payeuse \t: " << percentage(pricer_option1.Evaluate(swaption1)) << "\n";
-	std::cout << "Receveuse \t: " << percentage(pricer_option1.Evaluate(swaption2)) << "\n";
+	//Swap swap(0.05,1,courbe.times);
+	//Swaption swaption1(swap, true);
+	//Swaption swaption2(swap, false);
+
+
+	//std::cout << "=== Formule fermee ==="  << "\n";
+	//std::cout << "Payeur \t\t: " << percentage(pricer_swap.Evaluate(swap)) << "\n";
+	//std::cout << "Payeuse \t: " << percentage(pricer_option1.Evaluate(swaption1)) << "\n";
+	//std::cout << "Receveuse \t: " << percentage(pricer_option1.Evaluate(swaption2)) << "\n";
+	//std::cout << "=== Arbre trinomial ==="  << "\n";
+	//std::cout << "Payeuse \t: " << percentage(pricer_option2.Evaluate(swaption1)) << "\n";
+	//std::cout << "Receveuse \t: " << percentage(pricer_option2.Evaluate(swaption2)) << "\n";
+
+	//std::cout << "\n" << pricer_option2;
+	std::ofstream(std::ofstream("tree.gv", std::ios::trunc)) << pricer_option2;
+
+
+	////std::ofstream(std::ofstream("arbre.csv", std::ios::trunc)) << arbre;
 
 	std::getchar();
 	return 0;
--- Lib/Tools.h
+++ Lib/Tools.h
@@ -1,6 +1,14 @@
 #pragma once
+
 #include <iostream>
 #include <sstream>
+
+#include <math.h>
+
+#include <boost/math/special_functions/erf.hpp>
+#include <boost/math/distributions/normal.hpp>
+
+#define M_SQRT1_2 7.0710678118654752440E-1 //2^(-1/2)
 
 std::string percentage (double value, int precision = 3) {
 	std::ostringstream out (std::ostringstream::out);
@@ -11,6 +19,8 @@ std::string percentage (double value, int precision = 3) {
 	return out.str();
 };
 
+
+// todo : stop after a given time ?
 template <typename S, typename T, typename U>
 S dichotomicRootSearch(U f, T f_target, T precision, S min, S max) {
 	S target;
@@ -29,4 +39,43 @@ S dichotomicRootSearch(U f, T f_target, T precision, S min, S max) {
 		}
 	}
 	return target;
+};
+
+
+// erf(z) = 2/sqrt(pi) \quad_0^x exp(-t^2) dt
+// cdf(z) = 1/sqrt(2*pi) \quad_-inf^x exp(-t^2) dt
+class Gauss {
+public:
+	static double _erf(double){};
+	virtual double erf(double)=0;
+	virtual double cdf(double x) {return (1+erf(x*M_SQRT1_2))/2;};
+};
+
+// erf(0.01) = 0.0112834772 erf(3.7) = 0.9999998325
+// Abramowitz/Stegun: p299, |erf(z)-erf| <= 1.5*10^(-7)
+class AbramowitzStegunGauss : public Gauss {
+public:
+	virtual double erf(double x) {return _erf(x);};
+	static double _erf(double x) {
+		if (x<0) {
+			return -_erf(-x);
+		} else {
+			double y = 1.0 / ( 1.0 + 0.3275911 * x);   
+			return 1 - (((((
+				+ 1.061405429  * y
+				- 1.453152027) * y
+				+ 1.421413741) * y
+				- 0.284496736) * y 
+				+ 0.254829592) * y) 
+				* exp (-x * x);
+		}
+	};
+};
+
+// http://boost.org/doc/libs/1_48_0/boost/math/special_functions/erf.hpp
+class BoostGauss : public Gauss {
+public:
+	virtual double erf(double x) {return _erf(x);};
+	static double _erf(double x) {return boost::math::erf(x);}
+	virtual double cdf(double x) {boost::math::normal norm;return boost::math::cdf(norm, x);};
 };
